4ce85113cb2fb28f09c781761ef7d50bb80e2b4f
avashadhikari <avash.adhikari@berkeley.edu>
2023-05-06 21:23:46 -0700

    improve the sticky headers

--- proj/src/WhoAreThey/MeetMillennials.jsx
+++ proj/src/WhoAreThey/MeetMillennials.jsx
@@ -10,7 +10,7 @@ import { useEffect } from "react";
 import { Grid } from "@mui/material";
 
 const WhoAreThey = () => {
-  const stickyTopOffset = 200;
+  const stickyTopOffset = 0;
 
   useEffect(() => {
     AOS.init({
@@ -23,100 +23,102 @@ const WhoAreThey = () => {
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={2} style={{ verticalAlign: "center" }}>
-          <Sticky
-            top={stickyTopOffset}
-            bottomBoundary="#workforce-spending"
-            innerZ={9999}
-            className="center"
-          >
-            <h1 className="center text-4xl font-bold" id="header">
-              Meet Millennials
-            </h1>
-          </Sticky>
-        </Grid>
+      <Sticky
+        top={stickyTopOffset}
+        bottomBoundary="#workforce-spending"
+        innerZ={9999}
+        className="center"
+      >
+        <div
+          style={{
+            background: "white",
+            marginBottom: "50px",
+            paddingTop: "20px",
+          }}
+        >
+          <h1 className="center text-4xl font-bold" id="header">
+            Meet Millennials
+          </h1>
+        </div>
+      </Sticky>
 
-        <Grid item xs={10}>
-          <div
-            className="center snap-y snap-mandatory scroll-smooth"
-            data-aos="zoom-in-up"
-          >
-            <div id="content-intro" className="snap-always snap-center ">
-              <p className="insight">
-                Born between 1980 and 1996 in a rapidly changing world,
-                millennials have witnessed the world transcend like no other
-                generation.
-              </p>
+      <div
+        className="center snap-y snap-mandatory scroll-smooth"
+        data-aos="zoom-in-up"
+      >
+        <div id="content-intro" className="snap-always snap-center ">
+          <p className="insight">
+            Born between 1980 and 1996 in a rapidly changing world, millennials
+            have witnessed the world transcend like no other generation.
+          </p>
 
-              <img
-                src={fromTo}
-                alt="fromTo"
-                id="fromTo"
-                style={{ width: "100%" }}
-              />
+          <img
+            src={fromTo}
+            alt="fromTo"
+            id="fromTo"
+            style={{ width: "100%" }}
+          />
 
-              <br />
-              <p className="insight">
-                {" "}
-                With one of the largest generations at their helm, the economy
-                is on the brink of a transformation, driven by their distinct
-                experiences that will revolutionize the landscape of buying and
-                selling. <br />
-                Uncover the key events shaping their worldview and unlock the
-                untapped potential for businesses to thrive.
-              </p>
-            </div>
-          </div>
-          <div id="population" data-aos="zoom-in-up">
-            <img src={populationPercentage} />
-          </div>
-          <div id="workforce-spending" data-aos="zoom-in-up">
-            <img src={workforceSpending} />
-          </div>
-        </Grid>
-      </Grid>
+          <br />
+          <p className="insight">
+            {" "}
+            With one of the largest generations at their helm, the economy is on
+            the brink of a transformation, driven by their distinct experiences
+            that will revolutionize the landscape of buying and selling. <br />
+            Uncover the key events shaping their worldview and unlock the
+            untapped potential for businesses to thrive.
+          </p>
+        </div>
+      </div>
+      <div id="population" data-aos="zoom-in-up">
+        <img src={populationPercentage} />
+      </div>
+      <div id="workforce-spending" data-aos="zoom-in-up">
+        <img src={workforceSpending} />
+      </div>
 
       <div style={{ height: 200 }}></div>
       {/* MILLENNIAL ERA */}
-      <Grid container spacing={2}>
-        <Grid item xs={2}>
-          <Sticky
-            top={stickyTopOffset}
-            bottomBoundary="#timeline"
-            innerZ={9999}
-            className="center"
-          >
-            <h1 className="center text-4xl font-bold" id="header">
-              The Millennial Era
-            </h1>
-          </Sticky>
-        </Grid>
+      <Sticky
+        top={stickyTopOffset}
+        bottomBoundary="#timeline"
+        innerZ={9999}
+        className="center"
+      >
+        <div
+          style={{
+            background: "white",
+            marginBottom: "50px",
+            paddingTop: "20px",
+          }}
+        >
+          <h1 className="center text-4xl font-bold" id="header">
+            The Millennial Era
+          </h1>
+        </div>
+      </Sticky>
 
-        <Grid item xs={10}>
-          <div className="center snap-y snap-mandatory scroll-smooth">
-            <div id="timeline" className="snap-always snap-center">
-              <p className="insight">
-                Over the last four decades, Millennials have grown up in a
-                rapidly changing and digitally connected world, shaping their
-                worldview and values. Let’s take a look at the events that
-                shaped the unique worldview of this influential generation
-              </p>
-              <div style={{ height: 50 }}></div>
-              <iframe
-                src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1JUl7ELC-AImV7NfwoAAA9zl8PDWBgN1DS-R5hzmhWaM&font=Default&lang=en&initial_zoom=2&height=650"
-                width="100%"
-                height="650"
-                webkitallowfullscreen
-                mozallowfullscreen
-                allowFullScreen
-                frameBorder="0"
-                data-aos="zoom-in-up"
-              ></iframe>
-            </div>
-          </div>
-        </Grid>
-      </Grid>
+      <div className="center snap-y snap-mandatory scroll-smooth">
+        <div id="timeline" className="snap-always snap-center">
+          <p className="insight">
+            Over the last four decades, Millennials have grown up in a rapidly
+            changing and digitally connected world, shaping their worldview and
+            values. Let’s take a look at the events that shaped the unique
+            worldview of this influential generation
+          </p>
+          <div style={{ height: 50 }}></div>
+          <iframe
+            src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1JUl7ELC-AImV7NfwoAAA9zl8PDWBgN1DS-R5hzmhWaM&font=Default&lang=en&initial_zoom=2&height=650"
+            width="100%"
+            height="650"
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowFullScreen
+            frameBorder="0"
+            data-aos="zoom-in-up"
+          ></iframe>
+        </div>
+      </div>
     </>
   );
 };
