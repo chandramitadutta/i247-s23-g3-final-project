f287e4543e2c8ffe9a3d083795c76a75f1fac6d5
avashadhikari <avash.adhikari@berkeley.edu>
2023-05-06 22:15:43 -0700

    update default to no for living with parents viz

--- proj/src/LivingWithParents.jsx
+++ proj/src/LivingWithParents.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function LivingWithParents() {
   useEffect(() => {
-    const divElement = document.getElementById("viz1683435347354");
+    const divElement = document.getElementById("viz1683436384942");
     const vizElement = divElement.getElementsByTagName("object")[0];
     vizElement.style.width = "950px";
     vizElement.style.height = "750px";
@@ -13,12 +13,12 @@ export default function LivingWithParents() {
 
   return (
     <div>
-      <div className="tableauPlaceholder center-100" id="viz1683435347354">
+      <div className="tableauPlaceholder center-100" id="viz1683436384942">
         <noscript>
           <a href="#">
             <img
               alt="Dashboard 1 "
-              src="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;P9&#47;P9XHPWT8M&#47;1_rss.png"
+              src="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ri&#47;Risingnumberofmilennilaslivingwithparents&#47;Dashboard1&#47;1_rss.png"
               style={{ border: "none" }}
             />
           </a>
@@ -26,11 +26,16 @@ export default function LivingWithParents() {
         <object class="tableauViz" style={{ display: "none" }}>
           <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
           <param name="embed_code_version" value="3" />
-          <param name="path" value="shared&#47;P9XHPWT8M" />
+          <param name="site_root" value="" />
+          <param
+            name="name"
+            value="Risingnumberofmilennilaslivingwithparents&#47;Dashboard1"
+          />
+          <param name="tabs" value="no" />
           <param name="toolbar" value="yes" />
           <param
             name="static_image"
-            value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;P9&#47;P9XHPWT8M&#47;1.png"
+            value="https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ri&#47;Risingnumberofmilennilaslivingwithparents&#47;Dashboard1&#47;1.png"
           />
           <param name="animate_transition" value="yes" />
           <param name="display_static_image" value="yes" />
@@ -38,7 +43,6 @@ export default function LivingWithParents() {
           <param name="display_overlay" value="yes" />
           <param name="display_count" value="yes" />
           <param name="language" value="en-US" />
-          <param name="filter" value="publish=yes" />
         </object>
       </div>
     </div>
